66e82be26b276696fa2223e5b90ea157c0e1b01f
Unchun Yang <ucyang@ucyang.com>
2020-07-31 20:42:54 +0900

    10828

---
+++ baekjoon/problems/10828/stack.py
@@ -0,0 +1,21 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+s = []
+for _ in range(int(input())):
+    t = input().split()
+    if t[0] == "push":
+        s.append(t[1])
+    elif t[0] == "pop":
+        if len(s):
+            print(s[-1])
+            s.pop()
+        else:
+            print(-1)
+    elif t[0] == "size":
+        print(len(s))
+    elif t[0] == "empty":
+        print(0 if len(s) else 1)
+    elif t[0] == "top":
+        print(s[-1] if len(s) else -1)
