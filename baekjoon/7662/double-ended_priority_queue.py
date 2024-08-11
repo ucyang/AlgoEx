1c685f4cae725268c916a1a8a4ff7cf169091ecc
Unchun Yang <ucyang@ucyang.com>
2024-08-12 05:16:20 +0900

    Fix TLE after a re-marking

--- baekjoon/7662/double-ended_priority_queue.py
+++ baekjoon/7662/double-ended_priority_queue.py
@@ -1,33 +1,52 @@
 import sys
-import bisect
-from collections import deque
+from heapq import heappush, heappop
 
 input = lambda: sys.stdin.readline().rstrip()
 
+
+def minpop(d: dict, q: list) -> int:
+    while True:
+        i = heappop(q)
+        if i in d:
+            return i
+
+
+def maxpop(d: dict, q: list) -> int:
+    while True:
+        i = -heappop(q)
+        if i in d:
+            return i
+
+
 for _ in range(int(input())):
-    q = deque()
+    d = {}
+    minq, maxq = [], []
     count = {}
+
     for _ in range(int(input())):
         c, n = input().split()
         n = int(n)
+
         if c == "I":
-            if n in count:
-                count[n] += 1
+            if n in d:
+                d[n] += 1
             else:
-                bisect.insort(q, n)
-                count[n] = 1
-        elif c == "D" and q:
+                d[n] = 1
+
+            heappush(minq, n)
+            heappush(maxq, -n)
+        elif c == "D" and d:
             if n == 1:
-                if count[q[-1]] > 1:
-                    count[q[-1]] -= 1
-                else:
-                    count.pop(q.pop())
+                i = maxpop(d, maxq)
             elif n == -1:
-                if count[q[0]] > 1:
-                    count[q[0]] -= 1
-                else:
-                    count.pop(q.popleft())
-    if len(q):
-        print(q[-1], q[0])
+                i = minpop(d, minq)
+
+            if d[i] == 1:
+                del d[i]
+            else:
+                d[i] -= 1
+
+    if d:
+        print(maxpop(d, maxq), minpop(d, minq))
     else:
         print("EMPTY")
