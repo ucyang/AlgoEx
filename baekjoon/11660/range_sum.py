0d408d1e63844a008c6d517c3523e93357a476bc
Unchun Yang <ucyang@ucyang.com>
2024-09-27 22:03:01 +0900

    Refactor the solution of the problem 11660

--- baekjoon/11660/range_sum.py
+++ baekjoon/11660/range_sum.py
@@ -7,19 +7,14 @@ matrix = [list(map(int, input().split())) for _ in range(N)]
 ranges = [list(map(int, input().split())) for _ in range(M)]
 
 for i in range(N):
-    for j in range(N):
-        if j:
-            matrix[i][j] += matrix[i][j - 1]
-        elif i:
-            matrix[i][j] += matrix[i - 1][-1]
+    for j in range(1, N):
+        matrix[i][j] += matrix[i][j - 1]
 
 for x1, y1, x2, y2 in ranges:
     s = 0
     for x in range(x1 - 1, x2):
         if y1 > 1:
             s += matrix[x][y2 - 1] - matrix[x][y1 - 2]
-        elif x:
-            s += matrix[x][y2 - 1] - matrix[x - 1][-1]
         else:
             s += matrix[x][y2 - 1]
     print(s)
