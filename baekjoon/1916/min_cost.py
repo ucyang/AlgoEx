cebeb4693be127d56a8f85c89f4050f41467b531
Unchun Yang <ucyang@ucyang.com>
2024-09-29 21:55:12 +0900

    Refactor variable namings

--- baekjoon/1916/min_cost.py
+++ baekjoon/1916/min_cost.py
@@ -6,23 +6,24 @@ input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 M = int(input())
 
-adj_matrix = [[int(1e9)] * (N + 1) for _ in range(N + 1)]
+distance_matrix = [[int(1e9)] * (N + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
-    adj_matrix[i][i] = 0
+    distance_matrix[i][i] = 0
 
 for _ in range(M):
     start, end, cost = map(int, input().split())
-    adj_matrix[start][end] = min(adj_matrix[start][end], cost)
+    distance_matrix[start][end] = min(distance_matrix[start][end], cost)
 
 start, end = map(int, input().split())
-queue = deque([i for i, cost in enumerate(adj_matrix[start][1:], 1) if cost < int(1e9)])
+queue = deque([i for i, cost in enumerate(distance_matrix[start][1:], 1)
+               if cost < int(1e9)])
 
 while queue:
     v1 = queue.popleft()
-    for v2, cost in enumerate(adj_matrix[v1][1:], 1):
-        if adj_matrix[start][v1] + cost < adj_matrix[start][v2]:
-            adj_matrix[start][v2] = adj_matrix[start][v1] + cost
+    for v2, cost in enumerate(distance_matrix[v1][1:], 1):
+        if distance_matrix[start][v1] + cost < distance_matrix[start][v2]:
+            distance_matrix[start][v2] = distance_matrix[start][v1] + cost
             queue.append(v2)
 
-print(adj_matrix[start][end])
+print(distance_matrix[start][end])
