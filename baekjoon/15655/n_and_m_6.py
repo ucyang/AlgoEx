9d74eba11d626ce4e1a6ba9692bb1eaddd590579
Unchun Yang <ucyang@ucyang.com>
2024-11-17 02:59:21 +0900

    Refactor the solution of the problem 15655 and 15656

--- baekjoon/15655/n_and_m_6.py
+++ baekjoon/15655/n_and_m_6.py
@@ -3,5 +3,5 @@ from itertools import combinations
 N, M = map(int, input().split())
 numbers = sorted(map(int, input().split()))
 
-for p in combinations(range(N), M):
-    print(*[numbers[i] for i in p])
+for c in combinations(numbers, M):
+    print(*c)
--- baekjoon/15656/n_and_m_7.py
+++ baekjoon/15656/n_and_m_7.py
@@ -3,5 +3,5 @@ from itertools import product
 N, M = map(int, input().split())
 numbers = sorted(map(int, input().split()))
 
-for p in product(range(N), repeat=M):
-    print(*[numbers[i] for i in p])
+for p in product(numbers, repeat=M):
+    print(*p)
