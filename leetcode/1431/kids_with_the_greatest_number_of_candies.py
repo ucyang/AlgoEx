5a42aed9f341497c5dc41e869172744375963185
Unchun Yang <ucyang@ucyang.com>
2026-09-17 17:30:32 +0900

    Remove unnecessary imports and clean up code in LeetCode 1431

--- leetcode/1431/kids_with_the_greatest_number_of_candies.py
+++ leetcode/1431/kids_with_the_greatest_number_of_candies.py
@@ -1,8 +1,3 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         threshold = max(candies) - extraCandies
