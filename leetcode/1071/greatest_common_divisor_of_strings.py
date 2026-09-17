d7b1a9cd0db9963c25a0c8908ce027752d783751
Unchun Yang <ucyang@ucyang.com>
2026-09-17 17:28:48 +0900

    Optimize GCD logic in LeetCode 1071

--- leetcode/1071/greatest_common_divisor_of_strings.py
+++ leetcode/1071/greatest_common_divisor_of_strings.py
@@ -6,19 +6,20 @@ class Solution:
             s1, s2 = str2, str1
 
         n1, n2 = len(s1), len(s2)
+        n_gcd = gcd(n1, n2)
 
-        for d in range(1, n1 + 1):
-            if n1 % d == 0:
-                i = n1 // d
-                for j in range(i, n1, i):
-                    if s1[:i] != s1[j:j + i]:
+        for i in range(1, n1 + 1):
+            if n_gcd % i == 0:
+                n_d = n_gcd // i
+                for j in range(n_d, n1, n_d):
+                    if s1[:n_d] != s1[j:j + n_d]:
                         break
                 else:
-                    if n2 % i == 0:
-                        for j in range(0, n2, i):
-                            if s1[:i] != s2[j:j + i]:
+                    if n2 % n_d == 0:
+                        for j in range(0, n2, n_d):
+                            if s1[:n_d] != s2[j:j + n_d]:
                                 break
                         else:
-                            return s1[:i]
+                            return s1[:n_d]
 
         return ""
